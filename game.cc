40ddd157b7e8403ff25bfdb1968bfb842744fad6
Benja <benjamin.enrique.romero@estudiantat.upc.edu>
2026-04-16 08:40:27 +0200

    Subiendo archivos

---
+++ game.cc
@@ -0,0 +1,66 @@
+#include "game.hh"
+#include "assert.hh"
+using namespace pro2;
+
+Game::Game(int width, int height)
+    : mario_({width / 2, 150}),
+      platforms_{
+          Platform(100, 300, 200, 211),
+          Platform(0, 200, 250, 261),
+          Platform(250, 400, 150, 161),
+      },
+      finished_(false) {
+    assert(width > 0 && height > 0, "L'amplada i l'alcada del joc han de ser positives.");
+    for (int i = 1; i < 20; i++) {
+        platforms_.push_back(Platform(250 + i * 200, 400 + i * 200, 150, 161));
+    }
+}
+
+void Game::process_keys(pro2::Window& window) {
+    if (window.is_key_down(Keys::Escape)) {
+        finished_ = true;
+        return;
+    }
+}
+
+void Game::update_objects(pro2::Window& window) {
+    mario_.update(window, platforms_);
+}
+
+void Game::update_camera(pro2::Window& window) {
+    const Pt pos = mario_.pos();
+    const Pt cam = window.camera_center();
+
+    const int left = cam.x - window.width() / 4;
+    const int right = cam.x + window.width() / 4;
+    const int top = cam.y - window.height() / 4;
+    const int bottom = cam.y + window.height() / 4;
+
+    int dx = 0, dy = 0;
+    if (pos.x > right) {
+        dx = pos.x - right;
+    } else if (pos.x < left) {
+        dx = pos.x - left;
+    }
+    if (pos.y < top) {
+        dy = pos.y - top;
+    } else if (pos.y > bottom) {
+        dy = pos.y - bottom;
+    }
+
+    window.move_camera({dx, dy});
+}
+
+void Game::update(pro2::Window& window) {
+    process_keys(window);
+    update_objects(window);
+    update_camera(window);
+}
+
+void Game::paint(pro2::Window& window) {
+    window.clear(sky_blue);
+    for (const Platform& p : platforms_) {
+        p.paint(window);
+    }
+    mario_.paint(window);
+}
